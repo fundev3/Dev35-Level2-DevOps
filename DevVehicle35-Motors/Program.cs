791edd73ce207480276af516d462c7fbc692753b
Alejandro Javier Sanchez Roa <alejandro.sanchez@teamsoftware.com>
2022-05-13 08:45:02 -0400

    Add initial project

---
+++ DevVehicle35-Motors/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("DevVehicles 35 Fabrica de Autos");
+Console.WriteLine("===============================");
+
+string option = string.Empty;
+while (option != "q")
+{
+    Console.WriteLine();
+    Console.WriteLine("Select the type of vehicle you want to build:");
+    Console.WriteLine("q. Quit");
+    option = Console.ReadLine() ?? string.Empty;
+}
