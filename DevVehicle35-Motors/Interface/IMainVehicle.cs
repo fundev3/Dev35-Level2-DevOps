f9c1eaff38671e689ce1680ffd32f1cab435e117
Raul Camacho <Raul.Camacho@fundacion-jala.org>
2022-05-16 18:49:37 -0400

    Add SUV class

---
+++ DevVehicle35-Motors/Interface/IMainVehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace DevVehicle35_Motors
+{
+    internal interface IMainVehicle
+    {
+        public decimal Price { get; set; }
+
+        public int Speed { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public int Hapacity { get; set; }
+
+        public int NumberOfWheels { get; set; }
+
+        public string GetDescription();
+    }
+}
---
+++ DevVehicle35-Motors/Models/SUV.cs
@@ -0,0 +1,24 @@
+﻿namespace DevVehicle35_Motors.Models
+{
+    internal class SUV : IMainVehicle
+    {
+        public SUV()
+        {
+        }
+
+        public decimal Price { get; set; }
+
+        public int Speed { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public int Hapacity { get; set; }
+
+        public int NumberOfWheels { get; set; }
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
