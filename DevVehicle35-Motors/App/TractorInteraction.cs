c04cd24d0f8c9246408da0265e37e18fdac6b510
mary173 <mariananatv@gmail.com>
2022-05-19 18:51:58 -0400

    erase a comment in tractor interaction

--- DevVehicle35-Motors/App/TractorInteraction.cs
+++ DevVehicle35-Motors/App/TractorInteraction.cs
@@ -26,27 +26,8 @@ namespace DevVehicle35_Motors.App
 
                 Console.WriteLine("The horse power should be between 63000 and 70000");
                 Console.WriteLine("how much horse power do you want in the tractor?");
-            } 
+            }
             while (whileHelper == true);
-
-
-            //while (whileHelper)
-            //{
-            //    horsePower = int.Parse(Console.ReadLine());
-            //    if (horsePower >= 63000 && horsePower <= 70000)
-            //    {
-            //        whileHelper = false;
-            //    }
-            //
-            //    if (whileHelper == false)
-            //    {
-            //        break;
-            //    }
-            //
-            //    Console.WriteLine("The horse power should be between 63000 and 70000");
-            //    Console.WriteLine("how much horse power do you want in the tractor?");
-            //}
-
             Tractor tractor = new Tractor(horsePower);
             Console.WriteLine("Do you want warning lights?  1=Yes  2=No");
             int numberWarningLigths = int.Parse(Console.ReadLine());
