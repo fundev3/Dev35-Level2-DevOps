b9781e29ea59bb2e79661c85425ec408ae70aa6e
Daniel Vega <dani.vega.aguirre@gmail.com>
2022-06-08 09:47:35 -0400

    Finished step 3

---
+++ TodoApp/Startup.cs
@@ -0,0 +1,13 @@
+namespace TodoApp{
+
+
+    public void ConfigureServices(IServiceCollection services)
+	{
+		var config = new ServerConfig();
+		Configuration.Bind(config);    
+        var todoContext = new TodoContext(config.MongoDB);
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+	}
+
+
+}
