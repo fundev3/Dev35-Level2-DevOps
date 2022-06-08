106517849c1bda9ec8a9191b00150aa39b455e05
Camila13L <SCamilaLauraM@outlook.es>
2022-06-08 19:05:37 -0400

    .cs and configuration property added

--- TodoApp/Startup.cs
+++ TodoApp/Startup.cs
@@ -1,14 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+using Microsoft.Extensions.Configuration;
 namespace TodoApp{
 
+	public class Startup {
+		public Startup(IConfiguration configuration)//Constructor
+		{
+			Configuration = configuration;
+		}
+		public IConfiguration Configuration { get; }
 
-    public void ConfigureServices(IServiceCollection services)
-	{
-		var config = new ServerConfig();
-		Configuration.Bind(config);
-		var todoContext = new TodoContext(config.MongoDB);
-		var repo = new TodoRepository(todoContext);
-		services.AddSingleton<ITodoRepository>(repo);services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var config = new ServerConfig();
+			Configuration.Bind(config);
+
+			services.AddMvc();
+			var todoContext = new TodoContext(config.MongoDB);
+			var repo = new TodoRepository(todoContext);
+			services.AddSingleton<ITodoRepository>(repo); services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
 	}
+   
 
 
 }
