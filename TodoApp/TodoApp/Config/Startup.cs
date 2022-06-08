04b46a33bbfb6c09b0e4f9bd182656abac33b40f
Elizabeth Mamani <elizabeth.mamani@fundacion-jala.org>
2022-06-08 10:39:58 -0400

    Add ServerConfig.cs, MongoDbConfig.cs files and update Startup.cs

---
+++ TodoApp/TodoApp/Config/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var config = new ServerConfig();
+            Configuration.Bind(config);
+
+            services.AddMvc();
+        }
+    }
+}
