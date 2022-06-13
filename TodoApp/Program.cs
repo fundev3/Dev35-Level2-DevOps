5efb80f659f91fdee794b6af94af687fb2e11b0e
jeanpeep <santivanezjeanpaull@gmail.com>
2022-06-13 18:51:44 -0500

    Progra.cs updated to not use Startup.cs

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,25 +1,36 @@
+using TodoApp;
+using TodoApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Todo API",
+        Version = "v1",
+        Description = "Todo API tutorial using MongoDB",
+    });
+});
+var config = new ServerConfig();
+builder.Configuration.Bind(config);
+var todoContext = new TodoContext(config.MongoDB);
+var repo = new TodoRepository(todoContext);
+builder.Services.AddSingleton<ITodoRepository>(repo);
+builder.Services.AddMvc();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
