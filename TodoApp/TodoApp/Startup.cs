9c873b1bd38e6d7336a8f2ba2e1670ed669f250d
Elizabeth Mamani <elizabeth.mamani@fundacion-jala.org>
2022-06-13 10:00:22 -0400

    Add step 5.

--- TodoApp/TodoApp/Startup.cs
+++ TodoApp/TodoApp/Startup.cs
@@ -13,6 +13,20 @@ namespace TodoApp
             var repo = new TodoRepository(todoContext); 
             services.AddSingleton<ITodoRepository>(repo); 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Todo API",
+                    Version = "v1",
+                    Description = "Todo API tutorial using MongoDB",
+                });
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
