667691a61e8617acea5499eb237479b7724a3999
ariel10git <panoptico4@gmail.com>
2022-06-13 12:03:48 -0300

    Step 5

--- Dockerization/TodoApp/Startup.cs
+++ Dockerization/TodoApp/Startup.cs
@@ -6,6 +6,17 @@ namespace TodoApp
         Configuration.Bind(config);    
         var todoContext = new TodoContext(config.MongoDB);    
         var repo = new TodoRepository(todoContext);    
-        services.AddSingleton<ITodoRepository>(repo);services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        services.AddSingleton<ITodoRepository>(repo);
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new Info
+            {
+                Title = "Todo API",
+                Version = "v1",
+                Description = "Todo API tutorial using MongoDB",
+            });
+        });
+
     }
 }
