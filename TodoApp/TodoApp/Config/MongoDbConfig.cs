75d210f9404a30b8266491acc134274c552129d8
Raul Camacho <Raul.Camacho@fundacion-jala.org>
2022-06-14 13:04:21 -0400

    Finish compose app

--- TodoApp/TodoApp/Config/MongoDbConfig.cs
+++ TodoApp/TodoApp/Config/MongoDbConfig.cs
@@ -2,11 +2,11 @@
 {
     public class MongoDBConfig
     {
-		public string Database { get; set; } = "TodoDB";
-		public string Host { get; set; } = "mongo";
-		public int Port { get; set; } = 27017;
-		public string User { get; set; } = "root";
-		public string Password { get; set; } = "example";
+		public string Database { get; set; }
+		public string Host { get; set; }
+		public int Port { get; set; }
+		public string User { get; set; }
+		public string Password { get; set; }
 		public string ConnectionString
 		{
 			get
