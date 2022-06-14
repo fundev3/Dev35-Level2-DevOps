95bdcdd94b99f3c675d18bc3c4bb4362a311bf16
Carlos Zurita <czuritarojas@gmail.com>
2022-06-14 18:05:01 -0400

    add docker compose

---
+++ TodoApp/TodoApp/Program.cs
@@ -0,0 +1,37 @@
+using TodoApp;
+using TodoApp.Models;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Todo API",
+        Version = "v1",
+        Description = "Todo API tutorial using MongoDB",
+    });
+});
+var config = new ServerConfig();
+builder.Configuration.Bind(config);
+var todoContext = new TodoContext(config.MongoDB);
+var repo = new TodoRepository(todoContext);
+builder.Services.AddSingleton<ITodoRepository>(repo);
+builder.Services.AddMvc();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
