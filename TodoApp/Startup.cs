9635bead3b219c159251bab2c8b34a4a90d3c1bc
jeanpeep <santivanezjeanpaull@gmail.com>
2022-06-13 19:04:07 -0500

    dotnet Swashbuckle.AspNetCore package installed

--- TodoApp/Startup.cs
+++ TodoApp/Startup.cs
@@ -1,18 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using TodoApp.Models;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using TodoApp.Models;
 
-namespace TodoApp
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var config = new ServerConfig();
-            Configuration.Bind(config); 
-            var todoContext = new TodoContext(config.MongoDB); 
-            var repo = new TodoRepository(todoContext); 
-            services.AddSingleton<ITodoRepository>(repo); 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-    }
-}
+//namespace TodoApp
+//{
+//    public class Startup
+//    {
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            var config = new ServerConfig();
+//            Configuration.Bind(config); 
+//            var todoContext = new TodoContext(config.MongoDB); 
+//            var repo = new TodoRepository(todoContext); 
+//            services.AddSingleton<ITodoRepository>(repo); 
+//            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+//        }
+//    }
+//}
