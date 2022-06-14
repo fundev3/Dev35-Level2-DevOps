c81db0327bd70b84cc92384896d710e7efc9f0bb
Missael-JalaFund <missael.rodriguez@fundacion-jala.org>
2022-06-14 14:35:00 -0500

    Added and fixed Project

--- TodoApp/Models/ITodoContext.cs
+++ TodoApp/Models/ITodoContext.cs
@@ -1,6 +1,7 @@
 namespace TodoApp.Models
 {
-	using MongoDB.Driver;    public interface ITodoContext
+	using MongoDB.Driver;    
+	public interface ITodoContext
 	{
 		IMongoCollection<Todo> Todos { get; }
 	}
